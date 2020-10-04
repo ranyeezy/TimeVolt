93c170da96e44ade8efc0ff28efe4329f5f20889
atang816 <55515112+atang816@users.noreply.github.com>
2020-10-03 18:53:19 -0700

    Revised updates and deletes

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
 from .models import *
 from .forms import *
 # Create your views here.
@@ -7,6 +8,39 @@ from .forms import *
 def index(request):
     tasks = Task.objects.all()
 
-    form = TaskForms()
-    context = {'tasks': tasks, 'form':form}
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST) #Input by user posted on browser
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'tasks':tasks, 'form':form}
     return render(request, 'tasks/list.html', context)
+
+def updateTask(request, pk): #Updating a task, receives the task name
+    task = Task.objects.get(id=pk)
+
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()     #When user updates task or mark complete, will change display in browser
+            return redirect('/') #Redirects back to the main page after action
+
+
+    context = {'form':form}
+
+    return render(request, 'tasks/update_task.html', context)
+
+def deleteTask(request, pk):
+    item = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+
+    context = {'item':item}
+    return render(request, 'tasks/delete.html', context) #Pass the title task into delete page
