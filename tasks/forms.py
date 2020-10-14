31e48456df88f904f10ee42657d79670fc67c1c8
ranyeezy <ranielquesada@gmail.com>
2020-10-14 16:23:43 -0700

    bug fixes

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,10 +1,10 @@
 from django import forms
-from django.forms import ModelForm
+
 
 from .models import *
 
 
-class TaskForms(forms.ModelForm):
+class TaskForm(forms.ModelForm):
 
     class Meta:
         model = Task
