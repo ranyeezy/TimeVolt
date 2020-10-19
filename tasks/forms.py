cff5668c512494e8a642f0899f1eff4dafa0020c
ranyeezy <ranielquesada@gmail.com>
2020-10-18 19:01:45 -0700

    html/form changes

--- tasks/forms.py
+++ tasks/forms.py
@@ -8,4 +8,4 @@ class TaskForm(forms.ModelForm):
 
     class Meta:
         model = Task
-        fields = ['title', 'day' , 'timeStart', 'timeEnd']
+        fields = ['title', 'day' , 'due']
--- tasks/models.py
+++ tasks/models.py
@@ -18,9 +18,9 @@ DAYS = (
 class Task(models.Model):
     title = models.CharField(max_length=200)
     day = models.CharField(max_length= 1, choices=DAYS, default="0")
-    timeStart = models.TimeField()
-    timeEnd = models.TimeField()
     complete = models.BooleanField(default=False)
+    create = models.DateTimeField(auto_now_add=True)
+    due = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True, null=True)
 
     def __str__(self):
         return (self.title)
